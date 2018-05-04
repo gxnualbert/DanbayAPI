fd8256c53edffe6bbb8624c2af8db95887feecb2
gxnualbert <albert.chenwesoft@gmail.com>
2018-05-04 17:50:40 +0800

    修改了日志的记录方式。
    修改了日志格式：
    logging.Formatter('%(name)s: %(levelname)s %(message)s')
    
    修改了logging模块下的初始化文件下的callHandlers下的：
    （注意loggging 是python 自带的模块！！！！！）
    writeFlag=1
            while c:
                for hdlr in c.handlers:
                    # found = 1
                    found = found + 1
                    if record.levelno >= hdlr.level:
                        if writeFlag==2:
                                hdlr.handle(record)
                    writeFlag = writeFlag+1
    
    只有等于2的时候，才记录record，不然HTMLtest runner里面会重复写数据，就是同一个记录会写多次，所以我这里就改成，只有等于2的时候，才写上去

--- Util/logTool.py
+++ Util/logTool.py
@@ -20,42 +20,34 @@ class AddLog(object):
         pass
     @classmethod
     def Log(self,logmessage,loglevel="info"):
-        cwd = os.getcwd()
-        # logFolder=os.path.dirname(cwd)+ "\\API_Test_Report\\"+ "log_"+time.strftime("%Y-%m-%d")
-        logFolder=cwd+ "\\API_Test_Report\\"+ "log_"+time.strftime("%Y-%m-%d")
-        if not os.path.exists(logFolder):
-            os.mkdir(logFolder)
-        LOG_FILE =  logFolder +"\\"+ time.strftime('%Y-%m-%d %H_%M_%S', time.localtime(time.time())) + ".log"
-        # Logger=time.strftime('%Y-%m-%d %H%M%S', time.localtime(time.time()))
-        # handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1024 * 1024, backupCount=5)  # 实例化handler
-        # fmt = '%(asctime)s - %(filename)s:- %(message)s'
-        # # fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
-        #
-        # formatter = logging.Formatter(fmt)  # 实例化formatter
-        # handler.setFormatter(formatter)  # 为handler添加formatter
-        #
-        # logger = logging.getLogger(Logger)  # 获取名为tst的logger
-        # logger.addHandler(handler)  # 为logger添加handler
-        # logger.setLevel(logging.DEBUG)
-        logging.basicConfig(level=logging.DEBUG,
+        # cwd = os.getcwd()
+        # # logFolder=os.path.dirname(cwd)+ "\\API_Test_Report\\"+ "log_"+time.strftime("%Y-%m-%d")
+        # logFolder=cwd+ "\\API_Test_Report\\"+ "log_"+time.strftime("%Y-%m-%d")
+        # if not os.path.exists(logFolder):
+        #     os.mkdir(logFolder)
+        # LOG_FILE =  logFolder +"\\"+ time.strftime('%Y-%m-%d %H_%M_%S', time.localtime(time.time())) + ".log"
+
+        logging.basicConfig(level=logging.INFO,
                             format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                             datefmt='%Y-%m-%d %H:%M:%S',
-                            filename=LOG_FILE,
+                            filename="danbay",
                             filemode='w')
         #################################################################################################
         # 定义一个StreamHandler，将INFO级别或更高的日志信息打印到标准错误，并将其添加到当前的日志处理对象#
         console = logging.StreamHandler()
+        # console = logging.FileHandler(LOG_FILE)  # 写到文件中，不会写到HTML里面
         console.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+        # formatter = logging.Formatter('%(name)s: %(levelname)s %(message)s') %(name)s: %(levelname)s %(message)s 这一行可以控制log的格式 12s 表示12个空格
+        formatter = logging.Formatter('%(name)s: %(levelname)s %(message)s')
         console.setFormatter(formatter)
         logging.getLogger('').addHandler(console)
         #################################################################################################
-        if loglevel=="debug":
-            logging.debug(logmessage.decode("utf-8"))
-        elif loglevel=="info":
-            logging.info(logmessage.decode("utf-8"))
-        elif loglevel=="warning":
-            logging.warning(logmessage.decode("utf-8"))
+        # if loglevel=="debug":
+        #     logging.debug(logmessage.decode("utf-8"))
+        # elif loglevel=="info":
+        logging.info(logmessage.decode("utf-8"))
+        # elif loglevel=="warning":
+        #     logging.warning(logmessage.decode("utf-8"))
 
 
 # a=AddLog()
